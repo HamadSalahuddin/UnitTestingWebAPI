61a38834ab479060396fa5d2bd0b59d6f5a68aa0
Hamad Salahuddin <hamadsalahuddin@gmail.com>
2020-10-21 16:57:23 +0500

    Added MediaTypeFormatter Added Custom Message Handlers Added CustomAssemblyResolver

--- UnitTestingWebAPI/UnitTestingWebAPI.API/Filters/ArticlesReversedFilter.cs
+++ UnitTestingWebAPI/UnitTestingWebAPI.API/Filters/ArticlesReversedFilter.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http.Filters;
+using UnitTestingWebAPI.Domain;
 
 namespace UnitTestingWebAPI.API.Filters
 {
-    public class ArticlesReversedFilter
+    public class ArticlesReversedFilter: ActionFilterAttribute
     {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            var objectContent = actionExecutedContext.Response.Content as ObjectContent;
+            if(objectContent != null)
+            {
+                List<Article> articles = objectContent.Value as List<Article>;
+                if(articles.Any())
+                {
+                    articles.Reverse();
+                }
+            }
+        }
     }
 }
---
+++ UnitTestingWebAPI/UnitTestingWebAPI.API/HeaderAppenderHandler/EndRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace UnitTestingWebAPI.API.HeaderAppenderHandler
+{
+    public class EndRequestHandler : DelegatingHandler
+    {
+        async protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.RequestUri.AbsoluteUri.Contains("test"))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("Unit testing message handlers!")
+                };
+
+                var tsc = new TaskCompletionSource<HttpResponseMessage>();
+                tsc.SetResult(response);
+                return await tsc.Task;
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
