eaa57c0d483fc9526ef123413be4598225d153fd
Hamad Salahuddin <hamadsalahuddin@gmail.com>
2020-10-22 14:51:08 +0500

    Add Api Configuration and OWIN startup

---
+++ UnitTestingWebAPI/UnitTestingWebAPI.API/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Owin;
+using UnitTestingWebAPI.API.Controllers;
+using UnitTestingWebAPI.API.MediaTypeFormatters;
+using UnitTestingWebAPI.Data.Infrastructure;
+using UnitTestingWebAPI.Data.Repositories;
+using UnitTestingWebAPI.Services;
+
+[assembly: OwinStartup(typeof(UnitTestingWebAPI.API.Startup))]
+
+namespace UnitTestingWebAPI.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            var config = new HttpConfiguration();
+            config.Services.Replace(typeof(IAssembliesResolver), new CustomAssembliesResolver());
+            config.Formatters.Add(new ArticleFormatter());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Autofac configuration
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(typeof(BlogsController).Assembly);
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            //Repositories
+            builder.RegisterAssemblyTypes(typeof(BlogRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+            // Services
+            builder.RegisterAssemblyTypes(typeof(ArticleService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
